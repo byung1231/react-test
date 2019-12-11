5f4b257d738ca4669b0e45990b6000ffe54a9699
Byung Yoo <byung1231@gmail.com>
2019-12-10 21:32:39 -0500

    Updates

---
+++ precache-manifest.b701dcf53afb55f04d869497f4f76ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad73a29e9dbafa534ee11b500b42d7d",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "0342ef19a89346f5cc69",
+    "url": "/react-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "33641e74fb83a855cc13",
+    "url": "/react-test/static/js/2.7bc898ea.chunk.js"
+  },
+  {
+    "revision": "0342ef19a89346f5cc69",
+    "url": "/react-test/static/js/main.7983fc52.chunk.js"
+  },
+  {
+    "revision": "d88faedfaf3d758f47e5",
+    "url": "/react-test/static/js/runtime-main.27cd5638.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-test/static/media/logo.25bf045c.svg"
+  }
+]);
