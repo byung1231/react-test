6891c8d4dd59c18d9fec7a296c772407448c7db9
Byung Yoo <byung1231@gmail.com>
2019-12-15 02:17:53 -0500

    forms

--- src/Theta.js
+++ src/Theta.js
@@ -7,4 +7,20 @@ const Theta = () => (
    <p>Theta<br /> <img src={theta} className = "Theta-logo"/></p>
 );
 
+
+const forms = () => {
+<br/><br/>
+aa<br/>
+  <form>
+    <label>
+      Test forms
+      <input type="text" name="aaa"/>
+    </label>
+    <input type="text" name="bbb"/>
+  </form>
+
+
+}
+
 export default Theta
+export default forms
